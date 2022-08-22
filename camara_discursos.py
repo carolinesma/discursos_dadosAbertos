e3bb8244cc0b5e1f1f9a93de627c4411d6b0abc1
Caroline Alves <sma.caroline@ymail.com>
2022-08-22 00:19:32 -0300

    função para criar arquivo com sumario de deputados

--- camara_discursos.py
+++ camara_discursos.py
@@ -7,7 +7,7 @@ import pandas as pd
 from pandas import json_normalize
 from typing import Optional
 
-SETUP = {'legislatura': 56, 'path_discursos': 'Teste'}
+SETUP = {'legislatura': 56, 'path_discursos': 'Teste', 'path_sumario': 'Sumários'}
 
 def makeDir(path) -> None:
     try:
@@ -35,6 +35,11 @@ def json_to_dataFrame(json_data: json, tag: str) -> pd.DataFrame:
     elif tag == 'id':
         df = json_normalize(json_data['dados'], sep = ';')
         return df['id']
+    elif tag == 'sumario':
+        df = json_normalize(json_data['dados'], sep = ';')
+        df = df.drop(labels=['uri', 'uriPartido', 'idLegislatura', 'urlFoto', 'email'], axis=1, inplace=False, errors='raise')
+        print (df)
+        return df
     else:
         return None
 
@@ -44,7 +49,6 @@ def salvar_discurso(url, params, id):
     while next_page(res) is not None:
         res = request(next_page(res))
         discursos = pd.concat([discursos, json_to_dataFrame(res, 'discursos')], axis=0)
-
     discursos_csv = discursos.to_csv(os.path.join(SETUP['path_discursos'], str(SETUP['legislatura']), str(id)+'.csv'), sep = ';', encoding='utf-8', index=False)
     return 1
 
@@ -58,7 +62,18 @@ def deputados(legislatura: int) -> json:
 
     url = 'https://dadosabertos.camara.leg.br/api/v2/deputados'
 
-    return request(url)
+    return request(url, params)
+
+def deputados_sumario(res: json):
+    try:
+        sumario = json_to_dataFrame(res, 'sumario')
+        sumario.columns = ['Id', 'Nome', 'Partido', 'Estado']
+        sumario = sumario.reindex(columns = sumario.columns.tolist() + ['Profissão','Quantidade discursos', 'checksum'])
+        sumario_csv = sumario.to_csv(os.path.join(SETUP['path_sumario'], str(SETUP['legislatura'])+'.csv'), sep = ';', encoding='utf-8', index=False)
+    except:
+        print('Erro ao criar sumário de deputados')
+    else:    
+        print('Sumário de deputados criado com sucesso')
 
 def discursos(id: int, legislatura: int) -> json:
 
@@ -83,10 +98,12 @@ def saveJsonFile(data, fileName):
     with open(fileName+'.json', 'w', encoding="utf-8") as fp:
         json.dump(data, fp, indent = 4, ensure_ascii=False)
 
-
 def main():
     makeDir(os.path.join(SETUP['path_discursos'], str(SETUP['legislatura'])))
-    id = lista_id_deputados(deputados(56))
+    makeDir(os.path.join(SETUP['path_sumario']))
+    dep = deputados(SETUP['legislatura'])
+    deputados_sumario(dep)
+    id = lista_id_deputados(dep)
     
     for i in id:
         discursos(i, SETUP['legislatura'])
